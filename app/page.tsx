faa6f1d2f83dcc2bd9e1c84d130642c332e684b1
xeon <ahmad.yasni@gmail.com>
2025-09-19 19:13:47 +0800

    Fix missing Investors component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Capabilities from "@/components/Capabilities";
 import ScrumEnhancements from "@/components/ScrumEnhancements";
 import VideoGallery from "@/components/VideoGallery";
 import CTA from "@/components/CTA";
+import Investors from "@/components/Investors";
 
 export default function Page() {
   return (
